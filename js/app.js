9b5d3b4b16a40986bca95d14e17bb674ec20871e
ogbeniore <oreeboy@gmail.com>
2018-07-05 20:27:10 +0100

    removed starting animation

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
 var welcomeTimeline = anime.timeline();
 
-welcomeTimeline
-    .add({
-        targets: "#content",
-        width: "97vw",
-        height: "95vh",
-        easing: "easeInOutQuad"
-    })
-    .add({
-        targets: ".container",
-        opacity: 1,
-        easing: "easeInOutQuad"
-    });
+welcomeTimeline.add({
+    targets: ".container",
+    opacity: 1,
+    easing: "easeInOutQuad"
+});
